0091b120c2bb36d65fd2f8c6fd75b382df5448fa
Akash <87649977+Akash-yy@users.noreply.github.com>
2026-03-28 17:27:30 +0530

    Add VPC variables to variables.tf

---
+++ AWS_Hub_Spoke_Deployments/Modules/VPC/variables.tf
@@ -0,0 +1,7 @@
+variable "name" {}
+variable "cidr" {}
+variable "azs" { type = list(string) }
+
+variable "private_subnets" { type = list(string) }
+variable "public_subnets"  { type = list(string) }
+variable "tgw_subnets"     { type = list(string) }
