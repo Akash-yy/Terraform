2d641d0894b34f27a7f7aa9a9f7d752399de6f37
Akash <87649977+Akash-yy@users.noreply.github.com>
2026-04-16 11:17:49 +0530

    Add environment variable to VPC module
    Added 'environment' variable for configuration.

--- AWS_Hub_Spoke_Deployments/Modules/VPC/variables.tf
+++ AWS_Hub_Spoke_Deployments/Modules/VPC/variables.tf
@@ -3,5 +3,6 @@ variable "cidr" {}
 variable "azs" { type = list(string) }
 
 variable "private_subnets" { type = list(string) }
-variable "public_subnets"  { type = list(string) }
 variable "tgw_subnets"     { type = list(string) }
+
+variable "environment" {}
