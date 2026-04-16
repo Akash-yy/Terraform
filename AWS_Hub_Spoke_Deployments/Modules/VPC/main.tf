1c672a86f8d2eeabf7dfdb24656d615df3b037e1
Akash <87649977+Akash-yy@users.noreply.github.com>
2026-04-16 11:12:53 +0530

    Refactor VPC and subnet resources in main.tf

--- AWS_Hub_Spoke_Deployments/Modules/VPC/main.tf
+++ AWS_Hub_Spoke_Deployments/Modules/VPC/main.tf
@@ -1,5 +1,19 @@
 resource "aws_vpc" "this" {
   cidr_block = var.cidr
+
+  enable_dns_hostnames = true
+
+  lifecycle {
+    prevent_destroy = var.environment == "prod"
+  }
+
+  tags = {
+    Name = var.name
+  }
+}
+
+resource "aws_vpc" "this" {
+  cidr_block = var.cidr
   enable_dns_hostnames = true
 
   tags = {
@@ -7,48 +21,33 @@ resource "aws_vpc" "this" {
   }
 }
 
-# Private
+# Private Subnets
 resource "aws_subnet" "private" {
   count = length(var.private_subnets)
 
   vpc_id     = aws_vpc.this.id
   cidr_block = var.private_subnets[count.index]
-  availability_zone = var.azs[0]
+  availability_zone = var.azs[count.index]
 
   tags = {
     Name = "${var.name}-private-${count.index}"
   }
 }
 
-# Public
-resource "aws_subnet" "public" {
-  count = length(var.public_subnets)
-
-  vpc_id     = aws_vpc.this.id
-  cidr_block = var.public_subnets[count.index]
-  availability_zone = var.azs[0]
-
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "${var.name}-public-${count.index}"
-  }
-}
-
-# TGW
+# TGW Subnets
 resource "aws_subnet" "tgw" {
   count = length(var.tgw_subnets)
 
   vpc_id     = aws_vpc.this.id
   cidr_block = var.tgw_subnets[count.index]
-  availability_zone = var.azs[0]
+  availability_zone = var.azs[count.index]
 
   tags = {
     Name = "${var.name}-tgw-${count.index}"
   }
 }
 
-### Route tables
+# Route Table (private)
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.this.id
 
@@ -57,25 +56,10 @@ resource "aws_route_table" "private" {
   }
 }
 
+# Association
 resource "aws_route_table_association" "private" {
   count = length(aws_subnet.private)
 
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private.id
-}
-
-resource "aws_route_table" "public" {
-  count  = length(aws_subnet.public) > 0 ? 1 : 0
-  vpc_id = aws_vpc.this.id
-
-  tags = {
-    Name = "${var.name}-public-rt"
-  }
-}
-
-resource "aws_route_table_association" "public" {
-  count = length(aws_subnet.public)
-
-  subnet_id      = aws_subnet.public[count.index].id
-  route_table_id = aws_route_table.public[0].id
 }
