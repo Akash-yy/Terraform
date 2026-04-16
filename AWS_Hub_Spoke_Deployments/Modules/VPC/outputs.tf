0da0af0cac1307d16c5034c3396f85db1a168173
Akash <87649977+Akash-yy@users.noreply.github.com>
2026-04-16 11:18:37 +0530

    Add private route table ID output
    Add output for private route table ID in VPC module.

--- AWS_Hub_Spoke_Deployments/Modules/VPC/outputs.tf
+++ AWS_Hub_Spoke_Deployments/Modules/VPC/outputs.tf
@@ -2,6 +2,10 @@ output "vpc_id" {
   value = aws_vpc.this.id
 }
 
+output "private_rt_id" {
+  value = aws_route_table.private.id
+}
+
 output "private_subnets" {
   value = aws_subnet.private[*].id
 }
